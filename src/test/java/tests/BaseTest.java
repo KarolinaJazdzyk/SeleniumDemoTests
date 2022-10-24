e218089f73675cc8d989bb1a2369ef5275bd6ac0
Karolina Jazdzyk <karolina.mikula95@gmail.com>
2022-10-24 15:21:58 +0200

    DriverFactory BaseTest

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utils.DriverFactory;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = DriverFactory.getDriver();
+        driver.get("http://seleniumdemo.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
