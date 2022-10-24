688ffe5b0db26a2fc91b9e0a23adc2170541d16a
Karolina Jazdzyk <karolina.mikula95@gmail.com>
2022-10-24 17:52:22 +0200

    register Test assert + random email

--- src/test/java/pages/LoggedUserPage.java
+++ src/test/java/pages/LoggedUserPage.java
@@ -1,12 +1,25 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoggedUserPage {
+
+
+    @FindBy(xpath = "//a[text()='Logout']")
+    private WebElement logoutLink;
 
     private WebDriver driver;
 
     public LoggedUserPage(WebDriver driver){
-
+        PageFactory.initElements(driver,this);
+        this.driver=driver;
     }
+
+    public WebElement getLogoutLink() {
+        return logoutLink;
+    }
+
 }
--- src/test/java/tests/RegisterTest.java
+++ src/test/java/tests/RegisterTest.java
@@ -1,17 +1,25 @@
 package tests;
 
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoggedUserPage;
 
+import java.util.Random;
+
 public class RegisterTest extends BaseTest{
 
+    Random random= new Random();
+    int n= random.nextInt(1000);
     @Test
     public void registerUserTest(){
-        new HomePage(driver).openMyAccount()
-                .setEmail("tester567@tester.com")
-                .setPassword("123qwetester")
-                .clickRegister();
+        WebElement logoutLink= new HomePage(driver).openMyAccount()
+                     .setEmail("tester" + n + "@tester.com")
+                    .setPassword("123qwe!Tester")
+                    .clickRegister().getLogoutLink();
+
+        Assert.assertEquals(logoutLink.getText(),"Logout");
     }
 
 }
