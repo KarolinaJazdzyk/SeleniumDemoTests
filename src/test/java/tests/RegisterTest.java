40324d0f71f69f9ff787946b698ec9e91afa8bba
Karolina Jazdzyk <karolina.mikula95@gmail.com>
2022-10-24 17:34:26 +0200

    register Test

--- src/test/java/tests/RegisterTest.java
+++ src/test/java/tests/RegisterTest.java
@@ -2,12 +2,16 @@ package tests;
 
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.LoggedUserPage;
 
 public class RegisterTest extends BaseTest{
 
     @Test
     public void registerUserTest(){
-        new HomePage(driver).openMyAccount();
+        new HomePage(driver).openMyAccount()
+                .setEmail("tester567@tester.com")
+                .setPassword("123qwetester")
+                .clickRegister();
     }
 
 }
